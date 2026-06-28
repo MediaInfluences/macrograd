5340d4f626a73f8e0c9d81694811604305341e4c
Jonathan Kozlik <artificiallyunintelegent@gmail.com>
2026-06-28 11:02:29 -0400

    matrix: working matmul + broadcasting, add arithmetic operators
    - __matmul__ now works (tested in engine/test.py)
    - broadcasting reworked after the first design was wrong
    - add +, *, @ operator overloads for nicer usage
    
    Backward pass still to do.

--- engine/matrix.py
+++ engine/matrix.py
@@ -1,98 +1,83 @@
+"""Realized I designed _should_broadcast in the wrong way. FAAAAAH"""
+
 class Matrix():
 	def __init__(self, elements: list[list[int | float]], _inputs = (),  _op = ''):
 		assert isinstance(elements, list), "elements param must be of type list[list[int | float]]"
 		for i in elements:
 			for j in i:
-				assert isinstance(j, list), "elements param must be of type list[list[int | float]]"
-				for k in j:
-					assert isinstance(k, (int | float)), "elements param must be of type list[list[int | float]]"
-				assert len(j) == len(elements[0][0]), "elements param must be a non-jagged"
+				assert isinstance(j, (int | float)), "elements param must be of type list[list[int | float]]"
+				assert len(i) == len(elements[0]), "elements param must be a non-jagged"
 
 		self.elements = elements
 		self.grad = 0
 		self._dims = self._dimensions()
 		self._inputs = _inputs
 		self._op = _op	
-		self._backward = lambda : None
+		self._backward = lambda: None
 
 		
 	def _dimensions(self) -> tuple[int, int]:
 		return (len(self.elements), len(self.elements[0]))
 	
-	#Optimize this further for funsies (Maybe for the C prog later on look into bitwise ops)
-	def _should_broadcast(lhs_dims: tuple(int, int), rhs_dims: tuple(int, int)) -> str | bool:
-		lhs_a_one = 1 if lhs_dims[0] == 1 else 0
-		lhs_b_one = 1 if lhs_dims[1] == 1 else 0
-		rhs_a_one = 1 if rhs_dims[0] == 1 else 0
-		rhs_b_one = 1 if rhs_dims[1] == 1 else 0
-		lhrh_a_match = 1 if lhs_dims[0] == rhs_dims[0] else 0
-		lhrh_b_match = 1 if lhs_dims[1] == rhs_dims[1] else 0
+
+	#Optimize this further for funsies (Maybe for the C prog later on look into bitwise ops
+	def _should_broadcast(self, dims: tuple[int, int]) -> str | bool:
+		lhs_a_one = 1 if self._dims[0] == 1 else 0
+		lhs_b_one = 1 if self._dims[1] == 1 else 0
+		rhs_a_one = 1 if dims[0] == 1 else 0
+		rhs_b_one = 1 if dims[1] == 1 else 0
+		lhrh_a_match = 1 if self._dims[0] == dims[0] else 0
+		lhrh_b_match = 1 if self._dims[1] == dims[1] else 0
 		LHS = 0
 		RHS = 0
 
 		if lhs_a_one:
 			if lhrh_b_match:
-				return 'LHS' if not rhs_a_one
-			return 'LHS' if lhs_b_one
-		
+				if not rhs_a_one:
+					return 'LHS'
+				if not rhs_a_one:
+					return 'LHS' 
+			if lhs_b_one:
+				return 'LHS'		
 		elif not lhs_a_one:
 			if lhrh_a_match:
-				return 'RHS' if (not lhrh_b_match) and (lhs_b_one or rhs_b_one)
-			return 'RHS' if rhs_a_one and rhs_b_one
-
+				if (not lhrh_b_match) and (lhs_b_one or rhs_b_one):
+					return 'RHS'		
+			if rhs_a_one and rhs_b_one:
+				return 'RHS'
 		return False
-	
-	
-	#Probably something funky will arive here, note for when you debug backprop
-	#Found it, holy handling cases that will never exist maxxing
-	#Holy need to redo this thing to be right
-	def _broadcast(self, dims: tuple[int, int]) -> Matrix:
-		assert isinstance(dims, tuple), "dims param must be tuple[int, int]"
-
-		out = self
-		lhs_a_one = 1 if self._dims[0] == 1 else 0
-		rhs_b_one = 1 if self._dims[1] == 1 else 0
+			
 		
-		horizontal_broadcast = None
-		vertical_broadcast = None
-
+	def _broadcast(self, dims: tuple[int, int]):
+		assert isinstance(dims, tuple), "dims param must be tuple[int, int]"
 		for dimension in dims:
 			assert isinstance(dimension, int), "dims param must be tuple[int, int]"
+		assert ((self._dims[0] == 1) or (self._dims[1] == 1)), f"Cannot broadcast dims {self._dims} to {dims}" 
 
-		if lhs_a_one:
+		if self._dims[0] == 1:
 			horizontal_broadcast = [[1] for i in range(dims[0])]
 			horizontal_broadcast =  Matrix(horizontal_broadcast)
-			out = horizontal_broadcast @ out		
-			out = Matrix(out.elements, (horizontal_broadcast, self), '@')
+			out = horizontal_broadcast @ self		
+			
+			def _backward():
+				self.grad += horizontal_broadcast.transpose() @ out.grad
 	
-		if lhs_b_one:
-			vertical_broadcast = [1 for i in range(dims[1])]
-			vertical_broadcast = Matrix(horizontal_broadcast)
-			out = out @ vertical_broadcast
-			out = Matrix(out.elements, (self, vertical_broadcast), '@')
+		elif self._dims[1] == 1:
+			vertical_broadcast = [[1 for i in range(dims[1])]]
+			vertical_broadcast = Matrix(vertical_broadcast)
+			out = self @ vertical_broadcast
+			
+			def _backward():
+				self.grad += out.grad @ vertical_broadcast.transpose() 
 		
-		out = Matrix(out.elements, (self, ), 'broadcast')
-		ones_matrix = None
+		else:
+			return self
 	
-		if lhs_a_one:
-			if lhs_b_one:
-				ones_matrix = horizontal_broadcast @ vertical_broadcast
-				break
-
-			ones_matrix = horizontal_broadcast 
-
-		if lhs+b_one:
-			ones_matrix = vertical_broadcast
-
-		transposed_ones_matrix = ones_matrix.transpose()
-		def _backward():
-			self.grad += out.grad @ transposed_ones_matrix
-		
 		out._backward = _backward
 		return out 
 
 
-	def transpose(self) -> Matrix:
+	def transpose(self):
 		transpose = [[self.elements[i][j] for i  in range(self._dims[0])] for j in range(self._dims[1])]
 		out = Matrix(transpose, (self, ), 'T')
 		
@@ -103,19 +88,27 @@ class Matrix():
 		return out
 
 		
-	def hadamar_sum(self, other: Matrix) -> Matrix:
+	def hadamar_sum(self, other):
 		assert(isinstance(other, Matrix)), "Both operands of a hadamar sum must be a matrix"
-		maybe = should_broadcast(self._dims, other._dims)
-		assert(maybe, True), f"Cannot perform a Hadamar Sum on elements of dim {self._dims} and {other._dims}"
-		
-		broadcasted = None
+		maybe = self._should_broadcast(other._dims)
+		assert isinstance(maybe, str), f"Cannot perform a Hadamar Sum on elements of dim {self._dims} and {other._dims}"
 		
 		if maybe == 'LHS':
 			broadcasted = self._broadcast(other._dims)
-			out = Matrix([xi + yi for xi, yi in zip(broadcast._elements, other._elements)], (broadcasted, other), 'hadamar sum')
+			if (other._dims != broadcasted._dims):
+				bbroadcasted = broadcasted._broadcast(other._dims) 
+				out = Matrix([xi + yi for xi, yi in zip(bbroadcasted.elements, other.elements)], (bbroadcasted, other), '+')
+			else:
+				out = Matrix([xi + yi for xi, yi in zip(broadcasted.elements, other.elements)], (broadcasted, other), '+')
 		else:
-			broadcasted = other._broadcast(other._dims)
-			out = Matrix([xi + yi for xi, yi in zip(broadcast._elements, other._elements)], (self, broadcasted), 'hadamar sum')
+			broadcasted = other._broadcast(self._dims)
+			if (self._dims != broadcasted._dims):
+				bbroadcasted = broadcasted._broadcast(self._dims)
+				result  = [[xi + yi for xi, yi in zip(i, j)] for i, j in zip(bbroadcasted.elements, self.elements)]
+				out = Matrix(result, (self, broadcasted), '+')
+			else:
+				result  = [[xi + yi for xi, yi in zip(i, j)] for i, j in zip(broadcasted.elements, self.elements)]
+				out = Matrix(result, (self, broadcasted), (self, broadcasted), '+')
 
 		def _backward():
 			pass
@@ -124,38 +117,35 @@ class Matrix():
 		return out
 		
 
-	def hadamar_product(self, other: Matrix) -> Matrix:
+	def hadamar_product(self, other):
 		assert(isinstance(other, Matrix)), "Both operands of a hadamar product must be type Matrix"
-		maybe = should_broadcast(self._dims, other._dims)
-		assert(maybe, True), f"Cannot perform a Hadamar Product on elements of dim {self._dims} and {other._dims}"
-		
-		broadcasted = None
-		
+		maybe = self._should_broadcast(other._dims)
+		assert isinstance(maybe, str), f"Cannot perform a Hadamar Product on elements of dim {self._dims} and {other._dims}"
+			
 		if maybe == 'LHS':
 			broadcasted = self._broadcast(other._dims)
-			out = Matrix([xi * yi for xi, yi in zip(broadcast._elements, other._elements)], (broadcasted, other), 'hadamar product')
+			if (other._dims != broadcasted._dims):
+				bbroadcasted = broadcasted._broadcast(other._dims) 
+				out = Matrix([xi * yi for xi, yi in zip(bbroadcasted.elements, other.elements)], (bbroadcasted, other), '*')
+			else:
+				out = Matrix([xi * yi for xi, yi in zip(broadcasted.elements, other.elements)], (broadcasted, other), '*')
 		else:
-			broadcasted = other._broadcast(other._dims)
-			out = Matrix([xi * yi for xi, yi in zip(broadcast._elements, other._elements)], (self, broadcasted), 'hadamar product')
+			broadcasted = other._broadcast(self._dims)
+			if (self._dims != broadcasted._dims):
+				bbroadcasted = broadcasted._broadcast(self._dims)
+				result  = [[xi * yi for xi, yi in zip(i, j)] for i, j in zip(bbroadcasted.elements, self.elements)]
+				out = Matrix(result, (self, broadcasted), '*')
+			else:
+				result  = [[xi * yi for xi, yi in zip(i, j)] for i, j in zip(broadcasted.elements, self.elements)]
+				out = Matrix(result, (self, broadcasted), (self, broadcasted), '*')
 
 		def _backward():
 			pass
 
 		out._backward = _backward
-		return out
-
-	def __matmul__(self, other: Matrix) -> Matrix:
-		#if this works, holy aura
-		out = Matrix([[[sum(self.elements[i][j] * other.elements[j][k])] for j in range(self._dims[1]) for k in range(other._dims[0])] for i in range(self._dims[0])], (self, other), '@')
-
-		def _backwards:
-			pass
-
-		out._backward = _backward
-		return out
+		return out		
 	
-
-	def relu(self) -> Matrix:
+	def relu(self):
 		out  = Matrix([[i if i > 0 else 0 for i in self.elements[j]] for j in self.elements], (self, ), 'reLU')
 		
 		def _backward():
@@ -165,8 +155,8 @@ class Matrix():
 		return out			
 
 	
-	def max_margin_loss(self, truth: Matrix) -> Matrix:
-		assert(isinstance(truth, Matrix), "True values must be of type Matrix"
+	def max_margin_loss(self, truth):
+		assert(isinstance(truth, Matrix)), "True values must be of type Matrix"
 		loss = Matrix(sum(max(0, 1 - y_true * y_pred) for y_pred, y_true in zip(pred, true) for pred, true in zip(self.elements, truth.elements)), (self, truth), 'max margin loss')
 		
 		def _backward():
@@ -174,6 +164,7 @@ class Matrix():
 		
 		loss._backward = _backward
 		return loss
+
 	
 	def backwards(self) -> None:
 		topo = []
@@ -184,4 +175,28 @@ class Matrix():
 			topo.append(v)
 		
 		for node in reversed(topo):
-			node.backward()
+			node._backward()
+
+		
+	def __matmul__(self, other):
+		assert self._dims[1] == other._dims[0], f"Cannot perform matmul on operands of dims {self._dims} and {other._dims}" 
+		elements = [[sum(self.elements[i][j] * other.elements[j][k] for j in range(self._dims[1])) for k in range(other._dims[1])] for i in range(self._dims[0])]	
+		out = Matrix(elements, (self, other), '@')
+	
+		def _backward():
+			pass
+
+		out._backward = _backward
+		return out
+
+	def __add__(self, other):
+		return self.hadamar_sum(other)
+
+	def __radd__(self, other):
+		return self.hadamar_sum(other)
+
+	def __mul__(self, other):
+		return self.hadamar_product(other)
+
+	def __rmul__(self, other):
+		return self.hadamar_product(other)
--- engine/test.py
+++ engine/test.py
@@ -1,18 +1,66 @@
 from matrix import Matrix
 
-a = [[[1] for i in range(3)] for j in range(2)]
+a = [[1 for i in range(3)] for j in range(2)]
+b = [[1 for i in range(2)] for j in range(3)]
+d = [[1, 2, 3]]
+e = [[1], [2], [3]]
+ 
 a = Matrix(a)
+b = Matrix(b)
+c = Matrix([[5]])
+d = Matrix(d)
+e = Matrix(e)
 
 def print_matrix(egg): 
 	for i in egg.elements:
 		for j in i:
 			print(j, end=' ')
-		print()
+		print("\n")
 
+print("Transpose Test")
 
 print_matrix(a)
-print()
 a = a.transpose()
-print()
+
 print_matrix(a)
+a = a.transpose()
+
+print_matrix(a)
+
+print("a")
+print_matrix(a)
+
+print("b")
+print_matrix(b)
+
+print("a @ b")
+test  = a @ b
+print_matrix(test)
+
+print("Hadamar Sum Test")
+
+print("\n(n,m) + (1,1)")
+print("\na")
+print_matrix(a)
+
+print("\nc")
+print_matrix(c)
+
+print("\na + c")
+test = a + c
+
+print("\nresult")
+print_matrix(test)
+
+print("\n(n,m) + (1,1)")
+print("\na")
+print_matrix(a)
+
+print("\nc")
+print_matrix(c)
+
+print("\na + c")
+test = a + c
+print_matrix(test)
+
 
